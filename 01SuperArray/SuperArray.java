954232284ec887af417f0dd2e327ea843f693d55
annabelleg <agary110@gmail.com>
2014-11-12 23:43:04 -0500

    Update SuperArray.java

--- 01SuperArray/SuperArray.java
+++ 01SuperArray/SuperArray.java
@@ -1,2 +1,99 @@
 public class SuperArray{
+    private Object[] dataset;
+    private int numEls;
+
+    public SuperArray(){
+	this(10);
+    }
+    public SuperArray(int x){
+	dataset = new Object[x];
+	numEls = x;
+    }
+    public String toString(){
+	String stuff = "[ ";
+	for (int i = 0;i<dataset.length;i++){
+	    stuff += dataset[i] + " ";
+	}
+	return stuff + "]";
+    }
+    public void add(Object e){
+	if (dataset[size()]!=null){
+	    dataset = dataset.resize(size() + 1);
+	}
+	for (int i = 0; i < dataset.length ; i++){
+	    if (dataset[i]==null){
+	        dataset[i] = e;
+		break;
+	    }
+	}
+    }
+    public void add(int index, Object e){
+
+    }
+    public int size(){
+	return numEls;
+    }
+    public void resize(int newCapacity){
+	Object[] newArray = new Object[newCapacity];
+	numEls = newCapacity;
+	if (newCapacity > dataset.length){    
+	    for (int i = 0; i < dataset.length ; i++){
+		newArray[i] = dataset[i];
+	    }
+	}else{
+	    for (int i = 0; i < newCapacity ; i++){
+		newArray[i] = dataset[i];
+	    }
+	}
+        dataset = newArray;
+    }
+
+    public void clear(){
+	for (int i = 0; i < dataset.length ; i++){
+	    dataset[i] = null;
+	}
+    }
+    public Object get(int index){
+	if ( index < 0  || index >= size() ){
+	    throw new IndexOutOfBoundsException();
+	}else{
+	    return dataset[index];
+	}
+    }
+    public void set(int index, Object e){
+	if ( index < 0  || index >= size() ){
+	    throw new IndexOutOfBoundsException();
+	}else{
+	    dataset[index] = e;
+	}
+    }
+    public static void main(String[]args){
+	SuperArray a = new SuperArray();
+	SuperArray b = new SuperArray(5);
+	//	System.out.println(a.toString());
+	//	System.out.println(b.toString());
+	a.add(5);
+	b.add("seven");
+	a.add("hello");
+	b.add(78);
+	System.out.println(a.toString());
+	System.out.println(b.toString());
+	a.resize(3);
+	b.resize(10);
+	System.out.println(a.toString());
+	System.out.println(b.toString());
+	a.clear();
+	System.out.println(a.toString());
+	System.out.println(b.toString());
+	//	System.out.println(a.get(3));
+	System.out.println(b.get(0));
+	b.set(6,"buggabo");
+	System.out.println(b.toString());
+	//Example of ways to add Integer objects:
+	Integer x = new Integer(5);
+	SuperArray L = new SuperArray();
+	L.add(x);
+	L.add(new Integer(99));
+	System.out.println(L.toString());
+    }
 }
