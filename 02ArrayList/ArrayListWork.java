d3f663a820996234204d829712aa66024c4abf4c
annabelleg <agary110@gmail.com>
2014-11-13 21:17:27 -0500

    fixed collapseDuplicates, added randomize
    everything actually works now! woohoo

--- 02ArrayList/ArrayListWork.java
+++ 02ArrayList/ArrayListWork.java
@@ -1,17 +1,45 @@
-import java.until.*;
+import java.util.*;
 
-public class ArrayListWork{
+public class ArrayListMethods{
     
-    public void collapseDuplicates(ArrayList<Integer> L){
-    for (int i = 0; i < L.length -1 ; i++){
-        if (L[i] = L[i+1]) L.remove(i+1);
-      }
+    public static void collapseDuplicates(ArrayList<Integer> L){
+	int i = 0;
+	while ( i < L.size() -1){
+	    if (L.get(i) == L.get(i+1)){
+		L.remove(i+1);
+	    }else{
+		i++;
+	    }
+	}
+    }
+    
+    public static void randomize(ArrayList<Integer> L){
+	Random rand = new Random();
+	for (int i = 0; i < L.size(); i++){
+	    L.add(L.remove(rand.nextInt(L.size())));
+	}
     }
 
     public static void main(String[]args){
-    ArrayList<Integer> L = new ArrayList<Integer>(9,1,9,3,3,4);
-    L.toArray();
-    L.collapseDuplicates();
-    L.toArray();
+	ArrayList<Integer> L = new ArrayList<Integer>();
+	L.add(1);
+	L.add(2);
+	L.add(2);
+	L.add(3);
+
+	System.out.println(L);
+	collapseDuplicates(L);
+	System.out.println(L);
+
+	randomize(L);
+	System.out.println(L);
+
+	/*	ArrayList<Integer> M = new ArrayList<Integer>();
+	M.add(1);
+	M.add(2);
+	M.add(3);
+	M.add(4);
+	randomize(M);
+	System.out.println(M);*/
     }
 }
