e0f99165d3de3376b712a831f8afe1c40e1ff6b7
annabelleg <agary110@gmail.com>
2014-11-21 00:32:27 -0500

    Added more test cases

--- 03wordsearch/WordSearch.java
+++ 03wordsearch/WordSearch.java
@@ -1,16 +1,17 @@
 public class WordSearch{
     public static void main(String[]args){
 	WordGrid a = new WordGrid(8,8);
-	/*	System.out.println(a);
+	System.out.println(a);
 	System.out.println(a.addWordHorizontal("mello", 5,2));
 	System.out.println(a);
-	System.out.println(a.addWordVertical("jello", 3,8));
-	System.out.println(a);*/
-	System.out.println(a.addWordDiagonalDown("jello", 0,0));
+	System.out.println(a.addWordVertical("jello", 1,6));
 	System.out.println(a);
-	System.out.println(a.addWordDiagonalDown("jello", 2,6));
+	System.out.println(a.addWordDiagonalDown("hello", 0,0));
 	System.out.println(a);
-
+	System.out.println(a.addWordDiagonalUp("yellow", 6,0));
+	System.out.println(a);
+	/*	a.clear();
+		System.out.println(a);*/
 
 	//	System.out.println(a.getCols());
 	//	System.out.println(a.getRows());
