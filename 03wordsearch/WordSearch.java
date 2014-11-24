bff6d1f163f60eb327f7cc306363796744f1be0a
annabelleg <agary110@gmail.com>
2014-11-23 20:12:33 -0500

    testing out the new fit method

--- 03wordsearch/WordSearch.java
+++ 03wordsearch/WordSearch.java
@@ -1,7 +1,10 @@
+import java.util.*;
+import java.io.*;
+
 public class WordSearch{
     public static void main(String[]args){
-	WordGrid a = new WordGrid(8,8);
-	System.out.println(a);
+	WordGrid a = new WordGrid(15,15);
+	/*System.out.println(a);
 	System.out.println(a.addWordHorizontal("mello", 5,2));
 	System.out.println(a);
 	System.out.println(a.addWordVertical("jello", 1,6));
@@ -10,10 +13,31 @@ public class WordSearch{
 	System.out.println(a);
 	System.out.println(a.addWordDiagonalUp("yellow", 6,0));
 	System.out.println(a);
-	/*	a.clear();
+		a.clear();
 		System.out.println(a);*/
 
 	//	System.out.println(a.getCols());
 	//	System.out.println(a.getRows());
+	//	String a = "hello world";
+	WordGrid.fit("rubberduck",a);
+	WordGrid.fit("hello",a);
+	System.out.println(a);
+	WordGrid.fit("world",a);
+	System.out.println(a);
+	WordGrid.fit("peanut",a);
+	WordGrid.fit("love",a);
+	WordGrid.fit("annabelle",a);
+	WordGrid.fit("ruby",a);
+	System.out.println(a);
+	WordGrid.fit("queen",a);
+	WordGrid.fit("candle",a);
+	WordGrid.fit("candelabra",a);
+	WordGrid.fit("tenletters",a);
+	System.out.println(a);
+	WordGrid.fit("birthday",a);
+	WordGrid.fit("cow",a);
+	WordGrid.fit("hi",a);
+
+	System.out.println(a);
     }
 }
