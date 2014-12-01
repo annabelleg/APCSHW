a6a8d26ecd9203d5cabc6f6b6a65d99dc6b733f8
annabelleg <agary110@gmail.com>
2014-11-30 20:51:54 -0500

    fixed almost everything
    just need to refactor and fix seed code

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -5,21 +5,19 @@ public class Driver{
     public static void main(String[]args){
 	int rows;
 	int cols;
-	//	int randomSeed = null;
 	boolean fillrandletters = true;
 	if (args[0]!=null && args[1]!=null){
 	    rows = Integer.parseInt(args[0]);
 	    cols = Integer.parseInt(args[1]);
 	}else{
 	    System.out.println("Please specify how large you want your grid to be! Like this:\n      java Driver rows cols");
-	    return false;
 	}
 	if (args[3]=="1") fillrandletters = false;
 	WordGrid w = new WordGrid(rows, cols);
-	if(this.getSeed()!=null){  /*see more notes on this later*/
+	if (this.getSeed()!=null){ 
 	    w.setSeed(Integer.parseInt(args[2]));  
 	}
-	w.loadWordsFromFile("wordBank.txt", fillrandletters );
+	w.loadWordsFromFile("wordbank.txt", fillrandletters );
 	System.out.println( "Find these words:\n"+ w.wordsInPuzzle() );
 	System.out.println( w );
     }
