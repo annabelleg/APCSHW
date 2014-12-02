982b350711926c8879698e7283fd2e706518d726
annabelleg <agary110@gmail.com>
2014-12-02 00:45:06 -0500

    added add() codes, main
    this doesn't work--i get this error:
    Exception in thread "main" java.lang.NullPointerException
    	at java.lang.String.compareTo(String.java:1167)
    	at OrderedSuperArray.add(OrderedSuperArray.java:14)
    	at OrderedSuperArray.main(OrderedSuperArray.java:28)
    when i run (it compiles) though

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,3 +1,32 @@
 public class OrderedSuperArray extends SuperArray{
 
+    public OrderedSuperArray(){
+	super();
+    }
+    public OrderedSuperArray(int x){
+	super(10);
+    }
+    public void add(String s){
+	if (this.size() == 0){
+	    super.add(s);
+	}else{
+	    for (int i = 0; i < this.size(); i++){
+		if (s.compareTo(this.get(i)) < 1){
+		    super.add(i, s);
+		}
+	    }
+	    super.add(s);
+	}
+    }
+    /*  public String set(int index, String s){
+	String temp = this.get(index);
+	super.set(index,s);
+	
+	}*/
+    public static void main(String[]args){
+	OrderedSuperArray a = new OrderedSuperArray(1);
+	a.add("Hello");
+	a.add("Yolo");
+	System.out.println(a);
+    }
 }
