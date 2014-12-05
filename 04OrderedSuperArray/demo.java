95004339c3c391ba10330f894b241f43065eda28
annabelleg <agary110@gmail.com>
2014-12-05 13:20:28 -0500

    fixed this stuff

--- 04OrderedSuperArray/demo.java
+++ 04OrderedSuperArray/demo.java
@@ -1,16 +1,21 @@
+import java.util.Arrays;
 public class demo{
     public static void main(String[]args){
 	SuperArray a = new SuperArray(500000);
+	String[] b = new String[500000];
 	for (int i = 0; i < 500000; i++){
 	    a.set(i, ""+i);
+	    b[i] = ""+i;
 	}
-	if (args[0]=="select"){
-	    selectionSort(a);
+	if (args.length == 0){
+	    System.out.println("Please enter a type of sort");
+	}else if (args[0]=="select"){
+	    a.selectionSort();
 	}else if (args[0]=="insert"){
-	    insertionSort(a);
+	    a.insertionSort();
 	}else if (args[0]=="array"){
-	    Arrays.sort();
-	}else{
-	    System.out.println("Please enter a type of sort.");
+	    Arrays.sort(b);
+
 	}
     }
+}
