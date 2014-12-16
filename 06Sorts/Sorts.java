4d85e0c0b8a30214ea4ad1e0867ee5775aaeba98
annabelleg <agary110@gmail.com>
2014-12-16 13:23:31 -0500

    Update Sorts.java

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,24 +1,24 @@
-public class Sorts{
-    public static void bubble(int[]c){
-	for (int i = 0 ; i < c.length-1 ; i++){
-	    if (c[i] > c[i+1]){
-		int temp = c[i];
-		c[i] = c[i+1];
-		c[i+1] = temp;
+ public  void insertionSort(){
+	for(int i = 1; i < size(); i++){
+	    String value = get(i);
+	    int j = i - 1;
+	    while(j >= 0 && get(j).compareTo(value) < 1){
+		set(j + 1, get(j));
+		j--;
+	    }
+	    set(j + 1, value);
+	}
+    }
+    public void selectionSort(){
+	for (int i = 0; i < size(); i++){
+	    String temp = get(i);
+	    for (int x = 1; x <   size(); x++){
+		String smallest = get(0);
+		if (smallest.compareTo(get(x)) > 0){
+		    smallest = get(x);
+		}
+		set(i, smallest);
+		set(x, temp);
 	    }
 	}
     }
-    public static boolean isSorted(int[]c){
-	for (int i = 1; i < c.length ; i++){
-	    if (c[i] < c[i-1]) return false;
-	}
-	return true;
-    }
-    public static void main(String[]args){
-	int[] a = {5,6,3,8,0,-1,7,2};
-	Sorts.bubble(a);
-	//System.out.println(a);'
-	if (isSorted(a)) 
-	    System.out.println("true");
-    }
-}
