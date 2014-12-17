cbd3f2fd54fe45d8c9e62c15a267b981ada3b5bd
annabelleg <agary110@gmail.com>
2014-12-16 21:38:43 -0500

    got bubble to work yay

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,4 +1,25 @@
- public  void insertionSort(){
+public class Sorts{
+    public static void bubble(int[]c) {
+        boolean swapOccurred = true;                                         
+        while (swapped) {
+            swapped = false;
+            for (int i= 1; i < c.length; i ++) {
+                if (c[i-1] > c[i]) {
+                    int temp = c[i];
+                    c[i] = c[i-1];
+                    c[i-1] = temp;
+                    swapped = true;
+                }
+            }                           
+        }
+    }
+    static boolean isSorted(int[]c){
+	for (int i = 1; i < c.length ; i++){
+	    if (c[i] < c[i-1]) return false;
+	}
+	return true;
+    }
+    /*  public  void insertionSort(){
 	for(int i = 1; i < size(); i++){
 	    String value = get(i);
 	    int j = i - 1;
@@ -21,4 +42,12 @@
 		set(x, temp);
 	    }
 	}
+	}*/
+    public static void main(String[]args){
+	int[] a = {5,6,3,8,0,-1,7,2};
+	Sorts.bubble(a);
+	//System.out.println(a);'
+	if (isSorted(a)) 
+	    System.out.println("true");
     }
+}
